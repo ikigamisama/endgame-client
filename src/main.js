dfd87ba571632266298055feae4ee8c0e2e86e94
ikigamisama <kotascross22@gmail.com>
2022-05-15 19:53:15 +0800

    Finalize the 4v4 fix

--- src/main.js
+++ src/main.js
@@ -8,8 +8,8 @@ import 'element-theme-dark'
 import VueSocketIOExt from 'vue-socket.io-extended'
 import { io } from 'socket.io-client'
 
-// const socket = io('http://159.223.217.53:5000/')
-const socket = io('http://localhost:5000/')
+const socket = io('http://159.223.217.53:5000/')
+//const socket = io('http://localhost:5000/')
 
 Vue.config.productionTip = false
 Vue.use(VueSocketIOExt, socket)
